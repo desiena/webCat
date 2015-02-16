1a8830260cac5f77c26bfec49d07480351d01b9c
Ace DeSiena <desiena@oswego.edu>
2015-02-16 10:39:09 -0500

    Hello World with Swing

---
+++ src/webCatPack/HelloWorldSwing.java
@@ -0,0 +1,26 @@
+package webCatPack;
+
+/**
+ * Created by Ace DeSiena on 2/16/15.
+ */
+
+import javax.swing.*;
+
+public class HelloWorldSwing{
+    private static void createAndShowGUI(){
+            JFrame frame = new JFrame("HelloWorldSwing");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JLabel label = new JLabel("Hello, World!");
+            frame.getContentPane().add(label);
+            frame.pack();
+            frame.setVisible(true);
+    }
+    public static void main (String[] args){
+        javax.swing.SwingUtilities.invokeLater(new Runnable(){
+            public void run(){
+                createAndShowGUI();
+            }
+        });
+    }
+}
+
